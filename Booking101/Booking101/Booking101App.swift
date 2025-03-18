f6f8b7c36e9a8fe834263038ee8b39d3b197da40
aqyang28 <aqyang@unc.edu>
2025-03-18 09:48:03 -0400

    Created Xcode project.

---
+++ Booking101/Booking101/Booking101App.swift
@@ -0,0 +1,17 @@
+//
+//  Booking101App.swift
+//  Booking101
+//
+//  Created by Alex Yang on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Booking101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Booking101/Booking101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Booking101
+//
+//  Created by Alex Yang on 3/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
